5d64848bec9bdb93bf5937c57d1987c62d3f9755
solis_christoph <solis_christoph@bc439d21-e48f-43cc-9c43-3a02d8e75c1d>
2014-05-06 00:05:31 +0000

    Klasse DropdownMenu hinzugefügt. selektierter Gamename wird in variable gespeichert

--- src/ch/ntb/ini2/se/team2/bimaru/LevelSelectView.java
+++ src/ch/ntb/ini2/se/team2/bimaru/LevelSelectView.java
@@ -1,23 +1,59 @@
 package ch.ntb.ini2.se.team2.bimaru;
 
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class LevelSelectView extends JPanel {
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class LevelSelectView extends JPanel implements ActionListener{
 	private static final long serialVersionUID = -6070731239912690911L;
 	
 	//Globale Variable, welches den Namen des Ausgewählten Spiels enthält
 	//und an die anderen Klassen weitergeleitet wird
-	private String gamename;
+	private File selectedGame;
+	private File[] gameNames;
+	private JComboBox gameList;
+	
+	
 
 	public String getGamename() {
-		return gamename;
+		
+		return selectedGame.getName();
 	}
 
 	public LevelSelectView(BimaruGame bimaruGame) {
-		this.add(new JLabel("TODO: Level Selector"));
-		gamename="";
+		
+		this.add(new JLabel("Level Selector"));
+		GameNames();
+		DropdownMenu();
+		this.add(gameList);
+		
 	}
 
+	public void GameNames(){
+		gameNames = new File("//D:/Studium NTB/Semester 4/Softwareengineering/Bimaru/bin/games").listFiles();
+		for( File file : gameNames )
+		    System.out.println( file.getName() );
+	}
+	
+	
+	public void DropdownMenu(){
+		gameList = new JComboBox(gameNames);
+		gameList.setEditable(false);
+		gameList.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		 	
+		selectedGame =  (File) gameList.getSelectedItem();
+	         
+	         System.out.println(selectedGame.getName());
+		
+	}
 
 }
